d4743524e7207a42cd3fa61d3b3c0a5771f589ab
Nils Daumann <nils_daumann@slindev.com>
2014-11-15 15:44:15 +0100

    hektor position fixes.

--- Hacktopus/Classes/HPPlayer.cpp
+++ Hacktopus/Classes/HPPlayer.cpp
@@ -16,21 +16,21 @@ namespace HP
 	Player::Player()
 	{
 		_body = new RN::Billboard(RN::Texture::WithFile("Textures/hektor.png"));
-		_body->SetPosition(RN::Vector3(-70.0f, 280.0f, -4000.0f));
+		_body->SetPosition(RN::Vector3(0.0f, 300.0f, -4000.0f));
 		_body->GetMaterial()->SetLighting(false);
 		_body->GetMaterial()->SetAmbientColor(RN::Color::White());
 		_body->GetMaterial()->SetDepthWrite(false);
 		
 		_armRight = new AnimatableEntity();
 		_armRight->SetDefaultTexture(RN::Texture::WithFile("Textures/tentakelarm+0.png"));
-		_armRight->SetPosition(RN::Vector3(430.0f, 270.0f, -2000.0f));
+		_armRight->SetPosition(RN::Vector3(500.0f, 290.0f, -2000.0f));
 		_armRight->GetMaterial()->SetLighting(false);
 		_armRight->GetMaterial()->SetAmbientColor(RN::Color::White());
 		_armRight->GetMaterial()->SetDepthWrite(false);
 		
 		_armLeft = new AnimatableEntity(false);
 		_armLeft->SetDefaultTexture(RN::Texture::WithFile("Textures/tentakelarm+0.png"));
-		_armLeft->SetPosition(RN::Vector3(-540.0f, 290.0f, -2000.0f));
+		_armLeft->SetPosition(RN::Vector3(-470.0f, 310.0f, -2000.0f));
 		_armLeft->GetMaterial()->SetLighting(false);
 		_armLeft->GetMaterial()->SetAmbientColor(RN::Color::White());
 		_armLeft->GetMaterial()->SetDepthWrite(false);
